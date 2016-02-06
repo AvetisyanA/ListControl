e2958832a40e8cbe8ad42d68a601ac21efd18254
Artyom Avetisyan <artyom.avetisyan@yahoo.com>
2016-02-06 14:20:29 +0100

    save point

--- script/script.js
+++ script/script.js
@@ -1,13 +1,7 @@
 $(function() {
-    $('article.listBox').click(function (event) {
-      var eventTarget = $(event.target);
-      if (eventTarget.hasClass('listBoxItemHeader')) {
-          if (!eventTarget.parent().hasClass('listBoxItemExpanded')) {
-              $('section.listBoxItemExpanded').removeClass('listBoxItemExpanded');
-          }
-
-          eventTarget.parent().toggleClass('listBoxItemExpanded');
-      }
+    var header1 = $('#header1');
+    header1.click(function () {
+       $('#xz').toggleClass('animatedListBoxItemContent');
      })
 
 });
