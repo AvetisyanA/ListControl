213877bc54d1777d3806041f7ffab9a47b8a7957
Artyom Avetisyan <artyom.avetisyan@yahoo.com>
2016-02-06 22:42:06 +0100

    good point

--- script/script.js
+++ script/script.js
@@ -1,7 +1,19 @@
 $(function() {
-    var header1 = $('#header1');
-    header1.click(function () {
-       $('#xz').toggleClass('animatedListBoxItemContent');
+    $('#main').click(function (event) {
+      var eventTarget = $(event.target);
+      if (eventTarget.hasClass('listBoxItemHeader')) {
+          if (!eventTarget.parent().hasClass('listBoxItemExpanded')) {
+              $('section.container-column.listBoxItemExpanded').removeClass('listBoxItemExpanded');
+              $('section.container-column.listBoxItemExpanded').next().removeClass('exListBoxItemContent');
+              eventTarget.parent().addClass('listBoxItemExpanded');
+              eventTarget.next().addClass('exListBoxItemContent');
+              // console.log(eventTarget.next());
+          }
+          else {
+            eventTarget.parent().toggleClass('listBoxItemExpanded');
+            eventTarget.next().toggleClass('exListBoxItemContent');
+          }
+      }
      })
 
 });
